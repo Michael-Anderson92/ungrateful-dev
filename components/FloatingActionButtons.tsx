d5249b4c349b41a659f3fb44ee0ed14ab420afde
michaelanderson <Michael_Anderson92@hotmail.com>
2025-04-15 16:18:05 -0700

    added some placeholder photos for floating action buttons

--- components/FloatingActionButtons.tsx
+++ components/FloatingActionButtons.tsx
@@ -10,26 +10,51 @@ import NavigationIcon from '@mui/icons-material/Navigation';
 
 export default function FloatingActionButtons() {
   return (
-    <div className="flex mt-4 justify-center items-center space-x-8">
-        <div className="flex flex-col items-center text-center font-semibold">
-      <Fab href ="best-sellers" sx={{backgroundColor: 'var(--white)', marginBottom: 1 }}  size="large"  aria-label="">
-        +
-      </Fab>
-      Best Sellers
-      </div>
-      <div className="flex flex-col items-center text-center font-semibold">
-      <Fab href="/categories" sx={{backgroundColor: 'var(--white)', marginBottom: 1 }} size="large"  aria-label="">
-        +
-      </Fab>
-      Shop by Category
-      </div>
-      <div className="flex flex-col items-center text-center font-semibold">
-      <Fab href="become-a-seller" sx={{backgroundColor: 'var(--white)', marginBottom: 1 }} size="large"  aria-label="">
-        +
-      </Fab>
-      Top Sellers
-      </div>
-     
-    </div>
-  );
-}
+    
+    <div className="flex mt-4 justify-center items-center" style={{ width: '100%' }}>
+  <div
+    className="flex flex-col items-center text-center font-semibold"
+    style={{ flex: '0 0 33%', maxWidth: '33%' }}
+  >
+    <Fab
+      href="best-sellers"
+      sx={{ backgroundColor: 'var(--white)', backgroundImage: 'url(./backgrounds/best-sellers.webp)', backgroundSize: '150%', backgroundPosition: '-10px -5px', marginBottom: 1, }}
+      size="large"
+      aria-label=""
+    >
+      
+    </Fab>
+    Best Sellers
+  </div>
+  <div
+    className="flex flex-col items-center text-center font-semibold"
+    style={{ flex: '0 0 33%', maxWidth: '33%' }}
+  >
+    <Fab
+      href="/categories"
+      sx={{ backgroundColor: 'var(--white)', marginBottom: 1 }}
+      size="large"
+      aria-label=""
+    >
+      +
+    </Fab>
+    Categories
+  </div>
+  <div
+    className="flex flex-col items-center text-center font-semibold"
+    style={{ flex: '0 0 33%', maxWidth: '33%', overflowWrap: 'break-word' }}
+  >
+    {/* Make "Become a Seller" dynamic */}
+    <Fab
+      href="become-a-seller"
+      sx={{ backgroundColor: 'var(--white)', backgroundImage: 'url(./backgrounds/become-a-seller.png)', backgroundSize: '130%', backgroundPosition: '-8px -5px', marginBottom: 1 }}
+      size="large"
+      aria-label=""
+    >
+      +
+    </Fab>
+    Start your bar!
+  </div>
+</div>
+    );
+    }   
