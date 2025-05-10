c3b7d69b8e9ee8c5fdd8e5f7d94f1a311f3816f9
Michael-Anderson92 <Michael_Anderson92@hotmail.com>
2025-05-10 01:08:46 -0700

    made edits on about section

--- app/page.tsx
+++ app/page.tsx
@@ -70,8 +70,9 @@ export default function Page() {
           <h1 className="text-center text-4xl md:text-5xl pt-12 text-indigo-400 font-bold">
             The Ungrateful Developer
           </h1>
-          <p className="absolute top-8 left-8 text-amber-800 text-xl text-center">Stay tuned for frequent updates!</p>
-          <p className="text-center text-gray-400 mt-2 italic">*sigh* ...I suppose you can donate or whatever</p>
+          
+          
+          <p className="text-center text-gray-400 mt-4 mt-2 italic">A testament to the futility of digital panhandling</p>
 
           {/* Developer moved below title on mobile */}
           <div className="md:absolute static md:top-8 md:right-8 flex justify-center mt-8 md:mt-0">
@@ -225,32 +226,36 @@ export default function Page() {
 
         <div className="flex justify-center mt-8">
           <div className="w-full md:w-2/3 border border-indigo-700 rounded-lg bg-gray-900 p-6">
-            <h2 className="text-2xl text-indigo-300 mb-4 text-center">About This Developer</h2>
-            <div className="space-y-4 text-gray-300">
+            <h2 className="text-2xl text-indigo-300 mb-4 text-center">About the actual Developer</h2>
+            <div className="space-y-4 text-gray-300 mx-4 md:mx-16 lg:mx-32">
               <p className="italic text-center text-gray-400">*adjusts glasses with a sigh* <span className="text-rose-500">(just-kidding!)</span></p>
               <p className="text-center">
+                <br></br><br></br>Software Engineer, Digital Artist, and all-around Nerd. That pretty much sums me up. 
+                
+                A typical work day for me consists of waking up, drinking the morning brew, and dreaming big. 
+                
+                I'm unashamedly grateful to God for blessing me with my two beautiful boys, and the ability to do what I love. 
+                
+                I'm also overly humorous, and I frankly don't care. <span></span>Sue me!<br></br><br></br>
 
-                I'm actually quite the opposite in reality, and I <span className="text-rose-500">love</span> what I do!
+                I'm early in my journey as a developer, but I've adapted a <span className="text-green-500">growth mindset</span>, and I'm committed to learning, improving, and building things that help others and myself.
+                
+                Whether it's solving a tricky bug or shipping a new feature, I find real joy in the process and the progress.
 
-                <br></br><br></br>Thanks for stopping by! My name is Michael Anderson, and I genuinely enjoy coming into my office every day to change the world (or at least my code <span className="text-rose-500">(ง ͠° ͟ل͜ ͡°)ง</span>). I'm overly humorous, and I frankly don't care. Sue me!
-
-                <br></br><br></br>End of 2024, I dove headfirst into software engineering, and aside from spending time with my two beautiful boys, it's become my daily source of excitement and growth.
-
-                I'm early in my journey as a developer, but I've adapted a growth mindset, and I'm committed to learning, improving, and building things that matter. Whether it's solving a tricky bug or shipping a new feature, I find real joy in the process and the progress.
-
-                When I'm not writing code, you'll probably find me building LEGO contraptions with my kids, coaching baseball, orenjoying time with my family
+                When I'm not writing code, you'll probably find me building LEGO contraptions with my kids, coaching baseball, or enjoying time with my family.
 
                 I keep things pretty simple, but I really believe you can make a difference from your own home if you stick with it. I appreciate every bit of support and encouragement along the way.
 
-                Yours truly,
+                <br></br><br></br>
+
+                Yours truly, <br></br>
                 Michael Anderson
-              </p>
+              </p><br></br>
               
-              <p className="text-indigo-400 mt-4">
-                *sips coffee* Now that you know more about me than you probably wanted to,
-                feel free to throw some money at my problems. It won't fix them, but it'll
-                make me slightly less bitter about them.
-              </p>
+              <p className="text-center text-indigo-400 mt-4">
+  *sips coffee*  <br></br><br></br>
+  Unfortunately, character doesn't pay the bills.
+</p>
             </div>
           </div>
         </div>
